3d37133199e4767b53b8ad239c928843d04da21b
Yeongil-Park <p03479@naver.com>
2025-03-30 00:32:25 +0900

    e-book 1차차

---
+++ epub-viewer-project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EpubViewer from "./components/EpubViewer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <EpubViewer />
+    </div>
+  );
+}
+
+export default App;
